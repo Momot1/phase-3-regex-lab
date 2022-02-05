c3c93bb5585163bb536d5551c97963fd5dda7602
Mark Momot <markmomot03@gmail.com>
2022-02-04 22:11:34 -0500

    commit tree 95a06c3faaad48d9db56849ddf9fcd7491251aaf

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
 def starts_with_a_vowel?(word)
+    let tst = ""
+    tst = word[0].match(/[aeiou]/)
 
+    if tst != ""
+        true
+    end
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
